fbcbdd470371419a243e01aa27af524932868744
kwongio <rldh11111@naver.com>
2024-11-30 21:54:57 +0900

    refactor: 랜덤 번호 생성

--- src/main/java/lotto/service/SortedNumberGenerator.java
+++ src/main/java/lotto/service/SortedNumberGenerator.java
@@ -2,12 +2,13 @@ package lotto.service;
 
 
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.model.LottoRule;
 
 import java.util.List;
 
 public class SortedNumberGenerator {
 
     public List<Integer> generate() {
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return Randoms.pickUniqueNumbersInRange(LottoRule.BEGIN.getValue(), LottoRule.END.getValue(), LottoRule.SIZE.getValue());
     }
 }
