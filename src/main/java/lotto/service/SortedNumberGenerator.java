444ba006b5ac60c00e4db778c8dafb7d99cbdb2a
kwongio <rldh11111@naver.com>
2024-11-30 21:41:07 +0900

    feat: 랜덤 번호 생성

---
+++ src/main/java/lotto/service/SortedNumberGenerator.java
@@ -0,0 +1,13 @@
+package lotto.service;
+
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+public class SortedNumberGenerator {
+
+    public List<Integer> generate() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
+}
