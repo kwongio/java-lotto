cab4cfdaee9a8fe55b0b6e7efeb48ce5eb161edd
kwongio <rldh11111@naver.com>
2024-11-30 23:02:56 +0900

    fix: LottoValidator 수정

--- src/main/java/lotto/validator/LottoValidator.java
+++ src/main/java/lotto/validator/LottoValidator.java
@@ -2,35 +2,21 @@ package lotto.validator;
 
 import lotto.config.ErrorMessage;
 import lotto.model.Lotto;
-import lotto.model.LottoRule;
-
-import java.util.List;
+import lotto.config.LottoRule;
 
 public class LottoValidator {
 
     public static void validate(Lotto lotto) {
-        if (isLottoSize(lotto.getNumbers())) {
+        if (LottoRule.isNotSize(lotto.getNumbers())) {
             throw new IllegalArgumentException(ErrorMessage.LOTTO_SIZE_ERROR.toString());
         }
 
-        if (isDuplicate(lotto.getNumbers())) {
+        if (LottoRule.isDuplicate(lotto.getNumbers())) {
             throw new IllegalArgumentException(ErrorMessage.LOTTO_DUPLICATE_ERROR.toString());
         }
 
-        if (isRange(lotto.getNumbers())) {
+        if (LottoRule.isNotRange(lotto.getNumbers())) {
             throw new IllegalArgumentException(ErrorMessage.LOTTO_RANGE_ERROR.toString());
         }
-    }
-
-    private static boolean isLottoSize(List<Integer> numbers) {
-        return LottoRule.isSize(numbers);
-    }
-
-    private static boolean isDuplicate(List<Integer> numbers) {
-        return LottoRule.isDuplicate(numbers);
-    }
-
-    private static boolean isRange(List<Integer> numbers) {
-        return LottoRule.isRange(numbers);
     }
 }
