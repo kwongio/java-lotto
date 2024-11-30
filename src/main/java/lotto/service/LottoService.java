8503f6f99dc839b124de078eba4ef6ea98f160de
kwongio <rldh11111@naver.com>
2024-11-30 21:57:22 +0900

    feat: LottoService 추가

---
+++ src/main/java/lotto/service/LottoService.java
@@ -0,0 +1,27 @@
+package lotto.service;
+
+import lotto.model.Lotto;
+import lotto.model.LottoRule;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class LottoService {
+    private final SortedNumberGenerator sortedNumberGenerator;
+
+    public LottoService(SortedNumberGenerator sortedNumberGenerator) {
+        this.sortedNumberGenerator = sortedNumberGenerator;
+    }
+
+    public List<Lotto> purchase(int money) {
+        int quantity = LottoRule.getPurchaseQuantity(money);
+        return IntStream.rangeClosed(1, quantity)
+                .mapToObj(value -> new Lotto(sortedNumberGenerator.generate()))
+                .toList();
+    }
+
+    public void result(List<Lotto> lottos, List<Integer> winNumber, int bonusNumber) {
+
+
+    }
+}
